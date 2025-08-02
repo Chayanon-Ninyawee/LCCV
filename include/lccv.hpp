b2fc5aac86d707b4a5742c490aa045184cf6967b
Garfieldcmix <chayanon.nin@gmail.com>
2025-08-03 00:28:18 +0700

    Add ablity to gain access to ControlList to bypass the wrapper

--- include/lccv.hpp
+++ include/lccv.hpp
@@ -1,17 +1,20 @@
 #ifndef LCCV_HPP
 #define LCCV_HPP
 
-#include <mutex>
 #include <atomic>
-#include <pthread.h>
+#include <libcamera/controls.h>
+#include <mutex>
 #include <opencv2/opencv.hpp>
+#include <pthread.h>
 
 #include "libcamera_app.hpp"
 #include "libcamera_app_options.hpp"
 
-namespace lccv {
+namespace lccv
+{
 
-class PiCamera {
+class PiCamera
+{
 public:
     PiCamera();
     PiCamera(uint32_t id);
@@ -19,33 +22,35 @@ public:
 
     Options *options;
 
-    //Photo mode
+    // Photo mode
     bool startPhoto();
     bool capturePhoto(cv::Mat &frame);
     bool stopPhoto();
 
-    //Video mode
+    // Video mode
     bool startVideo();
     bool getVideoFrame(cv::Mat &frame, unsigned int timeout);
     void stopVideo();
 
-    //Applies new zoom options. Before invoking this func modify options->roi.
+    // Applies new zoom options. Before invoking this func modify options->roi.
     void ApplyZoomOptions();
+
+    libcamera::ControlList &getControlList();
 
 protected:
     void run();
 
-	std::unique_ptr<LibcameraApp> app;
+    std::unique_ptr<LibcameraApp> app;
     void getImage(cv::Mat &frame, CompletedRequestPtr &payload);
     static void *videoThreadFunc(void *p);
     pthread_t videothread;
     unsigned int still_flags;
-    unsigned int vw,vh,vstr;
-    std::atomic<bool> running,frameready;
+    unsigned int vw, vh, vstr;
+    std::atomic<bool> running, frameready;
     uint8_t *framebuffer;
     std::mutex mtx;
     bool camerastarted;
 };
 
-}
+}  // namespace lccv
 #endif
